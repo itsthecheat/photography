f1d2349ca72203ce9443f14ee7c473dcaf7b3e0b
Leslie Behum <llaurenb@gmail.com>
2018-03-16 11:11:59 -0400

    pages, layouts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,27 @@ console.log('\'Allo \'Allo!');
 $(function(){
   'use strict';
 
+  $('a.gallery').featherlightGallery({
+      previousIcon: '«',
+      nextIcon: '»',
+      galleryFadeIn: 200,
+      closeOnClick: 'background',
+      closeOnEsc: true,
+      openSpeed: 300
+    });
+
+
+  $.featherlightGallery.prototype.afterContent = function () {
+          var caption = this.$currentTarget.find('img').attr('alt');
+          this.$instance.find('.caption').remove();
+          $('<div class="caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
+  };
+
   $(document).ready(function () {
 
       $('#sidebarCollapse').on('click', function () {
           $('#sidebar').toggleClass('active');
       });
-
   });
 
   $('.hamburger').on('click', function () {
